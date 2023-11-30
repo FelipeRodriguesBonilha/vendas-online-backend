d56b4261f07cacb0739a45bd15ab43d98e10ea0d
FelipeRodriguesBonilha <133172138+FelipeRodriguesBonilha@users.noreply.github.com>
2023-11-30 18:51:21 -0300

    feat: connection with TypeORM, insert UserEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local'],
+  }), 
+  TypeOrmModule.forRoot({ //configura a conexão com o banco utilizando TypeORM
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    entities: [`${__dirname}/**/*.entity{.js,.ts}`], //comando para passar as entity's ao TypeORM, busca todas
+  }),
+  UserModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
 
+    constructor(private readonly userService: UserService) {}
+
     @Post()
-    async createUser(@Body() createUser: CreateUserDto){
-        return {
-            ...createUser,
-            password: undefined
-        }
+    async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {  
+        return this.userService.createUser(createUser);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userService.getAllUsers();
     }
 }

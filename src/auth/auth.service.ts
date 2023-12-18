a229ca87eacf8b26d8ce1aba45064f1138db3df3
FelipeRodriguesBonilha <133172138+FelipeRodriguesBonilha@users.noreply.github.com>
2023-12-18 19:56:24 -0300

    feat: create token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,20 @@ import { LoginDto } from './dtos/login.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLoginDto } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayloadDto } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
 
     constructor(
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private jwtService: JwtService
     ) {}
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
+    async login(loginDto: LoginDto): Promise<ReturnLoginDto> {
         const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
 
         const isMatch = await compare(loginDto.password, user?.password || '');
@@ -20,6 +25,9 @@ export class AuthService {
             throw new NotFoundException('Email or password invalid')
         }
 
-        return user;
+        return {
+            accessToken: await this.jwtService.signAsync({ ...new LoginPayloadDto(user) }),
+            user: new ReturnUserDto(user)
+        }
     }
 }

ab91308e48a1fe26aa041b492afb04b8b158178a
FelipeRodriguesBonilha <133172138+FelipeRodriguesBonilha@users.noreply.github.com>
2023-12-19 22:22:44 -0300

    feat: insert validation JWT

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Param, Post, Body, ValidationPipe, UsePipes } from '@nestjs
 import { AddressEntity } from './entities/address.entity';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
+import { UserType } from 'src/user/enum/userType.enum';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('address')
 export class AddressController {
@@ -10,6 +12,7 @@ export class AddressController {
 
     @UsePipes(ValidationPipe)
 
+    @Roles(UserType.Admin)
     @Post('/:userId')
     async createAddress(@Body() createAddressDto: CreateAddressDto, @Param('userId') userId: number): Promise<AddressEntity> {
         return this.addressService.createAddress(createAddressDto, userId);

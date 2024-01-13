bbee685157383adf6844d64165b105317b1db3fe
FelipeRodriguesBonilha <133172138+FelipeRodriguesBonilha@users.noreply.github.com>
2024-01-12 22:13:36 -0300

    feat: create getAddressesByUserId using TDD

--- src/address/__tests__/address.service.spec.ts
+++ src/address/__tests__/address.service.spec.ts
@@ -1,16 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CityService } from '../../city/city.service';
+import { cityMock } from '../../city/__mocks__/city.mock';
+import { UserService } from '../../user/user.service';
+import { userEntityMock } from '../../user/__mocks__/user.mock';
 import { Repository } from 'typeorm';
 import { AddressService } from '../address.service';
 import { AddressEntity } from '../entities/address.entity';
 import { addressMock } from '../__mocks__/address.mock';
-import { UserService } from '../../user/user.service';
-import { userEntityMock } from '../../user/__mocks__/user.mock';
-import { CityService } from '../../city/city.service';
-import { cityMock } from '../../city/__mocks__/city.mock';
 import { createAddressMock } from '../__mocks__/createAddress.mock';
 
-describe('CityService', () => {
+describe('AddressService', () => {
   let service: AddressService;
   let addressRepository: Repository<AddressEntity>;
   let userService: UserService;
@@ -24,18 +24,19 @@ describe('CityService', () => {
           provide: UserService,
           useValue: {
             findUserById: jest.fn().mockResolvedValue(userEntityMock),
-          }
+          },
         },
         {
           provide: CityService,
           useValue: {
             findCityById: jest.fn().mockResolvedValue(cityMock),
-          }
+          },
         },
         {
           provide: getRepositoryToken(AddressEntity),
           useValue: {
             save: jest.fn().mockResolvedValue(addressMock),
+            find: jest.fn().mockResolvedValue([addressMock]),
           },
         },
       ],
@@ -57,20 +58,41 @@ describe('CityService', () => {
   });
 
   it('should return address after save', async () => {
-    const address = await service.createAddress(createAddressMock, userEntityMock.id);
+    const address = await service.createAddress(
+      createAddressMock,
+      userEntityMock.id,
+    );
 
     expect(address).toEqual(addressMock);
   });
 
-  it('should return error exception in userService', async () => {
-    jest.spyOn(userService, 'findUserById').mockRejectedValue(new Error());
+  it('should return error if exception in userService', async () => {
+    jest.spyOn(userService, 'findUserById').mockRejectedValueOnce(new Error());
 
-    expect(service.createAddress(createAddressMock, userEntityMock.id)).rejects.toThrowError();
+    expect(
+      service.createAddress(createAddressMock, userEntityMock.id),
+    ).rejects.toThrowError();
   });
 
-  it('should return error exception in cityService', async () => {
-    jest.spyOn(cityService, 'findCityById').mockRejectedValue(new Error());
+  it('should return error if exception in cityService', async () => {
+    jest.spyOn(cityService, 'findCityById').mockRejectedValueOnce(new Error());
 
-    expect(service.createAddress(createAddressMock, userEntityMock.id)).rejects.toThrowError();
+    expect(
+      service.createAddress(createAddressMock, userEntityMock.id),
+    ).rejects.toThrowError();
+  });
+
+  it('should return all addresses to user', async () => {
+    const addresses = await service.getAddressesByUserId(userEntityMock.id);
+
+    expect(addresses).toEqual([addressMock]);
+  });
+
+  it('should return not found if not address registred', async () => {
+    jest.spyOn(addressRepository, 'find').mockResolvedValue(undefined);
+
+    expect(
+      service.getAddressesByUserId(userEntityMock.id),
+    ).rejects.toThrowError();
   });
 });
--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Param, Post, Body, ValidationPipe, UsePipes } from '@nestjs/common';
+import { Controller, Param, Post, Body, ValidationPipe, UsePipes, Get } from '@nestjs/common';
 import { AddressEntity } from './entities/address.entity';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { UserType } from '../user/enum/userType.enum';
 import { Roles } from '../decorators/roles.decorator';
 import { UserId } from '../decorators/userId.decorator';
+import { ReturnAddressDto } from './dtos/returnAddress.dto';
 
+@Roles(UserType.User, UserType.Admin)
 @Controller('address')
 export class AddressController {
 
@@ -13,9 +15,13 @@ export class AddressController {
 
     @UsePipes(ValidationPipe)
 
-    @Roles(UserType.User)
     @Post()
     async createAddress(@Body() createAddressDto: CreateAddressDto, @UserId() userId: number): Promise<AddressEntity> {
         return this.addressService.createAddress(createAddressDto, userId);
+    }
+
+    @Get()
+    async getAddressesByUserId(@UserId() userId: number): Promise<ReturnAddressDto[]> {
+        return (await this.addressService.getAddressesByUserId(userId)).map((address) => new ReturnAddressDto(address));
     }
 }

c93ba7317c77c0d2d3b6dd386cb3ce0bab905959
FelipeRodriguesBonilha <133172138+FelipeRodriguesBonilha@users.noreply.github.com>
2024-01-16 21:01:00 -0300

    feat: create categoryService

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { ReturnCategoryDto } from './dtos/returnCategory.dto';
+import { UserType } from 'src/user/enum/userType.enum';
+import { Roles } from 'src/decorators/roles.decorator';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+    constructor(private readonly categoryService: CategoryService){}
+
+    @Get()
+    async findAllCategories(): Promise<ReturnCategoryDto[]> {
+        return (await this.categoryService.findAllCategories()).map((category) => new ReturnCategoryDto(category));
+    }
+}
---
+++ src/category/dtos/returnCategory.dto.ts
@@ -0,0 +1,11 @@
+import { CategoryEntity } from "../entities/category.entity";
+
+export class ReturnCategoryDto {
+    id: number;
+    name: string;
+
+    constructor(category: CategoryEntity){
+        this.id = category.id;
+        this.name = category.name;
+    }
+}
